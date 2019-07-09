bc1f8e5901127660c65e671a725244bc668d4de1
Andres Mena <ballerlikemike23@gmail.com>
2019-07-08 22:36:05 -0400

    Styles to App

--- src/Hangman.js
+++ src/Hangman.js
@@ -85,7 +85,7 @@ class Hangman extends Component {
             ? this.generateButtons()
             : `You Lose!`}
         </p>
-        <button onClick={this.reset}>Restart?</button>
+        <button id='reset' onClick={this.reset}>Restart?</button>
       </div>
     );
   }
